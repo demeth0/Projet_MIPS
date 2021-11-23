c58b522067b1186e31631d05910c6291bd36926e
samdec666 <samdec66@gmail.com>
2021-11-23 09:51:58 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 int main(int argc, char const *argv[])
 {
+<<<<<<< Updated upstream
 	printf("hello world !--\n");
 	printf("edited\n");
+=======
+	printf("hello world !\n");
+	printf("edited test\n");
+>>>>>>> Stashed changes
 	return 0;
 }
