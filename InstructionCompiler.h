3ee0a150acd0d7f79a70c0d36a4810fb3124f26c
samdec666 <samdec66@gmail.com>
2021-12-14 09:59:58 +0100

    comm update

--- InstructionCompiler.h
+++ InstructionCompiler.h
@@ -16,27 +16,33 @@ typedef struct Instruction{
 
 /*
 Description:
-	prend une instruction texte d'une ligne et initialise la structure instruction
-	avec le type et la taille des blocs de données de l'instruction
+	prend une instruction texte d'une ligne et remplis la structure Instruction
+	avec l'id du code instruction défini dans operation_code_define et la taille de chaque
+	blocs de données de l'instruction met UNKNOW_ID dans la paramètre ID
+	de la structure Instruction si le code opération n'est pas reconnu.
 paramètre:
 	textInstruction - chaine de caractères contenant l'instruction
 	instruction - la struct inistruction qui recoit les informations déjà initialisées
 return:
 	void
 erreur:
-	si la chaine de caractère ne finit pas par \0
+	si la chaine de caractères ne finit pas par \0, ou si 
+	l'espace mémoire pour instruction n'est pas créée
 */
 void textInstructionToOpcode(char* textInstruction, Instruction* instruction);
 
 /*
 Description:
-	prend en paramètre une instruction et la chaine de caractère de l'instruction avec ses
-	opérandes et remplit les opérandes dans le code de la struct instruction
+	prend en paramètre une instruction et la chaine de caractères de l'instruction avec ses
+	opérandes et remplit les opérandes dans le code de la struct 
+	Instruction (écrit les valeurs des registres et valeurs immédiates dans le code 32bit).
 paramètre:
 	inst - l'instruction à remplir
 	inst_str - le texte qui contient l'instruction 
 return:
 	void
+erreur:
+	si inst_str ne finit pas par \0 ou si la struct Instruction n'est pas initialisée
 */
 void writeInstructionOperands(Instruction *inst, char *isnt_str);
 
