1d95e6f3ce38ee25a3a7be997b7a846cd517e6e1
Bitten54 <44749732+Bitten54@users.noreply.github.com>
2022-01-17 17:16:46 +0100

    commantaires

--- module_ram.h
+++ module_ram.h
@@ -4,8 +4,32 @@
 #include <stdio.h>
 #include "mips_structures.h"
 
+
+/*
+Description:
+	ecrit une valeur dans la ram a partir d'une adresse donnee
+parametre:
+	simulation - pour acceder a la ram
+	adresse - adresse en ram ou ecrire la valeur
+	value - valeur a ecrire en ram
+*/
 void writeRamADDR(Environment *simulation,DWord adresse,Byte value);
+
+/*
+Description:
+	lit une valeur dans la ram a partir d'une adresse donnee et retourne le resultat
+parametre:
+	simulation - pour acceder a la ram
+	adresse - adresse en ram ou ecrire la valeur
+*/
 Byte readRamADDR(Environment *simulation,DWord adresse);
+
+/*
+Description:
+	affiche la ram
+parametre:
+	simulation - pour acceder a la ram
+*/
 void afficher_ram(Environment *simulation);
 
 #endif
